9aa341a3906a84055ab70e9bdcae68ff772cdbbb
xpbcreator <xpbcreator@mac.hush.com>
2014-09-20 00:59:03 +0100

    Initial commit

---
+++ website_example/config.js
@@ -0,0 +1,17 @@
+var api = "http://pool.pebblecoin.io:8117";
+
+var coinUnits = 100000000;
+
+var poolHost = "pool.pebblecoin.io";
+
+var irc = "irc.freenode.net/#pebblecoin";
+
+var email = "xpbcreator@mac.hush.com";
+
+var cryptonatorWidget = []; //"XPB-BTC", "XPB-USD", "XPB-EUR", "XPB-GBP"];
+
+var easyminerDownload = ""; //zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = ""; //http://notyet.info/block/";
+
+var transactionExplorer = ""; //http://notyet.info/tx/";
